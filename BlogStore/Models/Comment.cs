2ddc0325882d9ccec236a26eb542ac4efe068427
LAPTOP-140RNLEU\Hansu kumar <hanshukumar8459@gmail.com>
2026-05-11 22:57:55 +0530

    Added post seeding and nullable Url

---
+++ BlogStore/Models/Comment.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogStore.Models
+{
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "The user name is required")]
+        [MaxLength(100, ErrorMessage = "user name cannot exceed 100 characters")]
+        public string UserName { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CommentDate { get; set; }
+        [Required]
+        public string Content { get; set; }
+        [ForeignKey("Post")]
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+
+    }
+}
---
+++ BlogStore/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlogStore.Models
+{
+    public class Post
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [MaxLength(400, ErrorMessage = "Title cannot exceed 400 characters")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Content is required")]
+        public string Content { get; set; }
+        public string? Url { get; set; }
+        [Required]
+        [MaxLength(100, ErrorMessage = "Length cannot exceed 100 characters")]
+        public string Author { get; set; }
+        public string FeatureImagePath { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime PublishedDate { get; set; } = DateTime.Now;
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        public ICollection<Comment> Comments { get; set; }
+    }
+}
